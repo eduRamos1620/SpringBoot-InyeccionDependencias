7953256493878a8683b179742e6e8924d42efc41
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-02-06 01:08:58 -0600

    Leyendo archivos JSON parte 1

---
+++ src/main/java/com/eduardo/springboot/di/app/springboot_di/repositories/ProductRepositoryJson.java
@@ -0,0 +1,45 @@
+package com.eduardo.springboot.di.app.springboot_di.repositories;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.eduardo.springboot.di.app.springboot_di.models.Product;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ProductRepositoryJson implements ProductRepository {
+
+    private List<Product> list;
+
+    public ProductRepositoryJson(){
+        Resource resource = new ClassPathResource("product.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
